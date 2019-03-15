f3bc51ebcd89c1942e4be1c893618b99bee78587
parkoon <par_koon@naver.com>
2019-03-15 18:47:08 +0900

    연습시작~

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import { Footer, Header } from './layouts'
+import Excercise from './excercises'
 
 class App extends Component {
   render() {
     return (
       <Fragment >
-        <Header></Header>
-        <Footer></Footer>
+        <Header />
+        <Excercise />
+        <Footer />
       </Fragment>
     );
   }
